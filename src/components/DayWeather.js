73ed9d6108cb373dfea09e53dd2cccd4a96f3d4e
Nadezda Gurska <n.gurska@cheil.com>
2020-02-07 12:28:30 +0000

    show days of the week

--- src/components/DayWeather.js
+++ src/components/DayWeather.js
@@ -6,6 +6,7 @@ import { WeatherContext } from "./appContext";
 function DayWeather(props) {
      const appContext = useContext(WeatherContext)
 		 const showDate = appContext.weather.list[props.day];
+		 const days = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat","Sun"]
 		// Setting the image
 		let imageName = "w-2" // default image name
 
@@ -16,12 +17,12 @@ function DayWeather(props) {
 			// set image if not full code found
 			imageName = "w-"+String(showDate.weather[0].id).split("")[0];
 		}
-console.log(showDate.dt)
+
 		const date = new Date(showDate.dt * 1000);
 		
 		return (
 			<div className="weatherApp__card">
-			  {(props.day!==0)?date.getDate()+"."+date.getMonth():""}
+			  {(props.day!=="0")?days[date.getDay()]:""}
 				<figure><img src={process.env.PUBLIC_URL + '/assets/images/' + imageName + '.png'} alt="" /><br />{showDate.weather[0].main}</figure>
 				<div className="weatherApp__card__content">
 					<div className="weatherApp__card__temp">{Math.round(showDate.main.temp)} &#176; {appContext.units==="metric"?"C":"F"}</div>
